1e4e74a375e3d51298375199d8eb9ec8110a9bca
2918351p@gmail.com <2918351p@gmail.com>
2023-07-15 16:34:01 +0500

    edited main page

--- lib/ui/main_page/main_page.dart
+++ lib/ui/main_page/main_page.dart
@@ -5,14 +5,55 @@ import 'package:film_list/ui/main_page/main_page_add.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class MainPage extends StatefulWidget {
+class MainPage extends StatelessWidget {
   const MainPage({super.key});
 
   @override
-  State<MainPage> createState() => _MainPageState();
+  Widget build(BuildContext context) {
+    return Stack(
+      children: const <Widget>[
+        MainPageBackground(),
+        MainPageFrontground(),
+      ],
+    );
+  }
 }
 
-class _MainPageState extends State<MainPage> {
+class MainPageBackground extends StatelessWidget {
+  const MainPageBackground({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Colors.pinkAccent,
+            Colors.deepPurpleAccent,
+            Colors.blue,
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MainPageFrontground extends StatefulWidget {
+  const MainPageFrontground({
+    super.key,
+  });
+
+  @override
+  State<MainPageFrontground> createState() => _MainPageFrontgroundState();
+}
+
+class _MainPageFrontgroundState extends State<MainPageFrontground> {
   bool isList = true;
   @override
   Widget build(BuildContext context) {
@@ -26,91 +67,73 @@ class _MainPageState extends State<MainPage> {
           );
         }
         final data = state.data;
-        return Stack(
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height,
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [
-                    Colors.pinkAccent,
-                    Colors.deepPurpleAccent,
-                    Colors.blue,
-                  ],
-                ),
-              ),
+        return ColorFiltered(
+          colorFilter: ColorFilter.mode(
+            Theme.of(context).canvasColor,
+            BlendMode.srcOut,
+          ),
+          child: Scaffold(
+            backgroundColor: Colors.transparent,
+            appBar: AppBar(
+              backgroundColor: Colors.transparent,
+              surfaceTintColor: Colors.transparent,
+              scrolledUnderElevation: 0,
+              title: const Text("Film List"),
+              centerTitle: true,
+              actions: [
+                IconButton(
+                    onPressed: () {}, icon: const Icon(Icons.adb_rounded))
+              ],
             ),
-            ColorFiltered(
-              colorFilter: ColorFilter.mode(
-                  Theme.of(context).canvasColor, BlendMode.srcOut),
-              child: Scaffold(
-                backgroundColor: Colors.transparent,
-                appBar: AppBar(
+            // drawer: const MainPageDrawer,
+            body: CustomScrollView(
+              slivers: <Widget>[
+                SliverAppBar(
+                  leading: null,
+                  floating: true,
                   backgroundColor: Colors.transparent,
                   surfaceTintColor: Colors.transparent,
+                  elevation: 0,
                   scrolledUnderElevation: 0,
-                  title: const Text("Film List"),
-                  centerTitle: true,
                   actions: [
                     IconButton(
-                        onPressed: () {}, icon: const Icon(Icons.adb_rounded))
+                        onPressed: () {}, icon: const Icon(Icons.search)),
+                    IconButton(
+                        onPressed: () {
+                          isList = !isList;
+                          setState(() {});
+                        },
+                        icon: isList == true
+                            ? const Icon(Icons.grid_view_sharp)
+                            : const Icon(Icons.list))
                   ],
                 ),
-                // drawer: const MainPageDrawer,
-                body: CustomScrollView(
-                  slivers: <Widget>[
-                    SliverAppBar(
-                      leading: null,
-                      floating: true,
-                      backgroundColor: Colors.transparent,
-                      surfaceTintColor: Colors.transparent,
-                      elevation: 0,
-                      scrolledUnderElevation: 0,
-                      actions: [
-                        IconButton(
-                            onPressed: () {}, icon: const Icon(Icons.search)),
-                        IconButton(
-                            onPressed: () {
-                              isList = !isList;
-                              setState(() {});
-                            },
-                            icon: isList == true
-                                ? const Icon(Icons.grid_view_sharp)
-                                : const Icon(Icons.list))
-                      ],
-                    ),
-                    data.isEmpty
-                        ? const SliverAppBar(
-                            title: Text("There is nothing!"),
-                            leading: null,
-                            centerTitle: true,
-                          )
-                        : isList == true
-                            ? ListViewVariable(data: data)
-                            : GridViewVariable(data: data),
-                  ],
-                ),
-                floatingActionButton: FloatingActionButton(
-                  backgroundColor: Colors.transparent,
-                  elevation: 0,
-                  shape: RoundedRectangleBorder(
-                    side: const BorderSide(),
-                    borderRadius: BorderRadius.circular(16)
-                  ),
-                  onPressed: () {
-                    showDialog(
-                      context: context,
-                      builder: (context) => const MainPageAdd(),
-                    );
-                  },
-                  child: const Icon(Icons.add),
-                ),
-              ),
+                data.isEmpty
+                    ? const SliverAppBar(
+                        title: Text("There is nothing!"),
+                        leading: null,
+                        centerTitle: true,
+                      )
+                    : isList == true
+                        ? ListViewVariable(data: data)
+                        : GridViewVariable(data: data),
+              ],
             ),
-          ],
+            floatingActionButton: FloatingActionButton(
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              shape: RoundedRectangleBorder(
+                  side: const BorderSide(),
+                  borderRadius: BorderRadius.circular(16)),
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) => const MainPageAdd(),
+                );
+              },
+              child: const Icon(Icons.add),
+            ),
+          ),
         );
       },
     );
